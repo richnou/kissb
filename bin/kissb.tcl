90e544a5eb476888a2408a7b951aeb7904320105
richnou <leys.richard@gmail.com>
2024-06-17 12:00:13 +0200

    Core: added extension utility and reordered plugin file names for proper loading order

--- bin/kissb.tcl
+++ bin/kissb.tcl
@@ -3,7 +3,8 @@ package require kiss
 
 
 ## Load local Kiss build
-foreach buildFile {kiss.b kiss.kb} {
+set args $argv
+foreach buildFile {kiss.b kiss.kb kiss.build} {
     if {[file exists $buildFile]} {
         source $buildFile
         break
@@ -13,17 +14,31 @@ foreach buildFile {kiss.b kiss.kb} {
 
 ## Run target
 if {[llength $argv] == 0 } {
-    puts "No targets provided"
+    log.warn "No targets provided"
     foreach target [kiss::targets::listTargets] {
         puts "- $target"
     }
 } else {
     set target [lindex $argv 0]
-    log.info "Running target: $target"
 
-    # Remove target from args
-    set argv [lrange $argv 1 end]
+    ## Run Target or command
+    ###########
+    if {[string range $target 0 0]=="."} {
 
-    # Run Target
-    kiss::targets::run $target {*}$argv
+        set cmd [string range $target 1 end]
+        log.info "Running command: $cmd"
+        [$cmd]
+
+    } elseif {[string range $target 0 0]!="-"} {
+
+        log.info "Running target: $target"
+
+        # Remove target from args
+        set argv [lrange $argv 1 end]
+
+        # Run Target
+        kiss::targets::run $target {*}$argv
+
+    }
+    
 }
