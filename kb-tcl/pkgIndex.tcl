1f9e6b025ca76f027a12bc354c95ac79ac594be7
richnou <leys.richard@gmail.com>
2025-05-28 20:10:16 +0200

    Updated lib pkgIndex for latest .kb -> .tcl renames

--- kb-tcl/pkgIndex.tcl
+++ kb-tcl/pkgIndex.tcl
@@ -1,47 +1,45 @@
 set ::kissDir $dir
 package ifneeded kissb                      1.0 [list foreach f [concat [lsort [glob $dir/kiss/kiss.plugin*.kb]] [list $dir/info.tcl] [list $dir/globals.tcl]] {source $f}]
 package ifneeded kissb.internal.update      1.0 [list source $dir/kiss/kiss.update.tcl]
-package ifneeded kissb.internal.tls         1.0 [list source $dir/kiss/kiss.tls.tcl]
+package ifneeded kissb.internal.tls         1.0  [list source $dir/kiss/kiss.tls.tcl]
 
 
-package ifneeded kissb.i18n                 1.0 [list source $dir/i18n/i18n.plugin.kb]
-
-package ifneeded kissb.git                  1.0 [list source $dir/git/git.plugin.kb]
-
-package ifneeded kissb.liquibase            1.0 [list source $dir/liquibase/liquibase.plugin.kb]
+package ifneeded kissb.i18n                 1.0  [list source $dir/i18n/i18n.plugin.kb]
+package ifneeded kissb.git                  1.0  [list source $dir/git/git.plugin.kb]
+package ifneeded kissb.liquibase            1.0  [list source $dir/liquibase/liquibase.plugin.kb]
 
 
-package ifneeded kissb.python3              1.0 [list source $dir/python3/python3.plugin.kb]
-package ifneeded kissb.mkdocs               1.0 [list source $dir/mkdocs/mkdocs.plugin.kb]
-package ifneeded kissb.netlify              1.0 [list source $dir/netlify/netlify.plugin.kb]
+package ifneeded kissb.python3              1.0  [list source $dir/python3/python3.plugin.kb]
+package ifneeded kissb.mkdocs               1.0  [list source $dir/mkdocs/mkdocs.plugin.kb]
+package ifneeded kissb.netlify              1.0  [list source $dir/netlify/netlify.plugin.kb]
 
-package ifneeded kissb.nodejs               1.0 [list source $dir/nodejs/nodejs.plugin.kb]
+package ifneeded kissb.nodejs               1.0  [list source $dir/nodejs/nodejs.plugin.kb]
 
-package ifneeded kissb.builder.container    1.0 [list source $dir/builder/builder.container.plugin.tcl]
-package ifneeded kissb.builder.rclone       1.0 [list source $dir/builder/builder.rclone.plugin.kb]
-package ifneeded kissb.docker               1.0 [list source $dir/containers/docker.plugin.kb]
-package ifneeded kissb.podman               1.0 [list source $dir/containers/podman.plugin.kb]
+package ifneeded kissb.builder.container    1.0  [list source $dir/builder/builder.container.plugin.tcl]
+package ifneeded kissb.builder.rclone       1.0  [list source $dir/builder/builder.rclone.plugin.tcl]
+package ifneeded kissb.docker               1.0  [list source $dir/containers/docker.plugin.tcl]
+package ifneeded kissb.podman               1.0  [list source $dir/containers/podman.plugin.tcl]
 
 
-package ifneeded kissb.tclkit               1.0  [list source $dir/tclkit/tclkit.plugin.kb]
-package ifneeded kissb.tcl9.kit              1.0 [list source $dir/tclkit/tclkit9.plugin.tcl]
+package ifneeded kissb.tclkit               1.0  [list source $dir/tclkit/tclkit.plugin.tcl]
+package ifneeded kissb.tcl9.kit             1.0  [list source $dir/tclkit/tclkit9.plugin.tcl]
 
-package ifneeded kissb.reuse               1.0 [list source $dir/licensing/reuse.plugin.tcl]
+package ifneeded kissb.reuse                1.0  [list source $dir/licensing/reuse.plugin.tcl]
 
 
 ## EDA Packages
 ####################
-package ifneeded kissb.eda.f                1.0 [list source $dir/eda/eda.f.plugin.tcl]
-package ifneeded kissb.eda.cocotb           1.0 [list source $dir/eda/cocotb/cocotb.plugin.tcl]
-package ifneeded kissb.eda.verilator        1.0 [list source $dir/eda/verilator/verilator.plugin.tcl]
+package ifneeded kissb.eda.f                1.0  [list source $dir/eda/eda.f.plugin.tcl]
+package ifneeded kissb.eda.cocotb           1.0  [list source $dir/eda/cocotb/cocotb.plugin.tcl]
+package ifneeded kissb.eda.verilator        1.0  [list source $dir/eda/verilator/verilator.plugin.tcl]
 
 ## Java and JEE Packages
 #####################
-package ifneeded kissb.java                 1.0 [list source $dir/java/java.plugin.kb]
-package ifneeded kissb.coursier             1.0 [list source $dir/java/coursier/coursier.plugin.kb]
-package ifneeded kissb.gradle               1.0 [list source $dir/java/gradle/gradle.plugin.kb]
-package ifneeded kissb.maven                1.0 [list source $dir/java/maven/maven.plugin.kb]
-package ifneeded kissb.ivy                  1.0 [list source $dir/java/ivy/ivy.plugin.kb]
+package ifneeded kissb.java                 1.0  [list source $dir/java/java.plugin.kb]
+package ifneeded kissb.coursier             1.0  [list source $dir/java/coursier/coursier.plugin.kb]
+package ifneeded kissb.gradle               1.0  [list source $dir/java/gradle/gradle.plugin.kb]
+package ifneeded kissb.maven                1.0  [list source $dir/java/maven/maven.plugin.kb]
+package ifneeded kissb.ivy                  1.0  [list source $dir/java/ivy/ivy.plugin.kb]
 
 
 package ifneeded kissb.kotlin               1.0 [list source $dir/java/kotlin/kotlin.plugin.kb]
