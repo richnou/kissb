4c1e2db600623cb0861061caa993c13bbff755a7
richnou <leys.richard@gmail.com>
2025-06-07 17:46:40 +0200

    Main: updated extra lib and package folders
    KISSB_LIBPATH can be set from env to add folder from which to load
    .lib.tcl files
    KISSB_PACKAGEPATH can be set from env to add folder from which packages
    can be found

--- kb-tcl/globals.tcl
+++ kb-tcl/globals.tcl
@@ -6,3 +6,9 @@ vars.set kissb.projectFolder [pwd]
 vars.set kissb.buildDir ${kissb.projectFolder}/.kb
 
 vars.set kissb.distribution portable
+
+# KISSB_LIBPATH can be set to add folders where .lib.tcl from users will be search for
+vars.define kissb.libpath ""
+
+# KISSB_PACKAGEPATH can be set to add folders where packages will be searched for.
+vars.define kissb.packagepath ""
