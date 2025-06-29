c06b6c9a9c37fe5d46c1b17745f88fae6d771e4c
richnou <leys.richard@gmail.com>
2025-06-29 14:37:30 +0200

    Dev: added script to download the dist1 tcl9 distribution localy

---
+++ vendor/copy_dist1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+
+version=250627
+
+wget https://kissb.s3.de.io.cloud.ovh.net/tcl9/dist1/${version}/tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version}.tar.gz
+rm -Rf tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version}
+tar xvaf tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version}.tar.gz
+rm tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version}.tar.gz
