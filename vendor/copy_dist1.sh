c8a802eacf65b802412677e41926c8a6d74e8e50
richnou <leys.richard@gmail.com>
2025-06-30 14:56:52 +0200

    Local TCL9: Updated dist1 to 250630 required for tdom package

--- vendor/copy_dist1.sh
+++ vendor/copy_dist1.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #
 
-version=250627
+version=250630
 
 wget https://kissb.s3.de.io.cloud.ovh.net/tcl9/dist1/${version}/tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version}.tar.gz
 rm -Rf tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version}
 tar xvaf tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version}.tar.gz
 rm tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version}.tar.gz
+
+# Relink
+rm -f tcl9 
+ln -s tcl9-dist1-x86_64-redhat-linux-rhel8-9.0.1-${version} tcl9
