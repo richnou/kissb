a07de92b061f2883029012ca3ccf7d09f8f2bf6b
richnou <leys.richard@gmail.com>
2025-10-02 14:19:11 +0200

    EDA: added experimental GHDL support, with llvm ghdl kissb build

---
+++ kb-tcl/eda/ghdl/ghdl.plugin.tcl
@@ -0,0 +1,64 @@
+package provide kissb.eda.ghdl 1.0
+
+
+namespace eval ghdl {
+
+    vars.define ghdl.runtime "kissb-llvm" -doc "Runtime selected, default is to download KISSB LLVM build (kissb-llvm)"
+    vars.define ghdl.version "5.1.1"
+    vars.define ghdl.installFolder false
+
+    kiss::toolchain::register ghdl {
+
+        switch ${::ghdl.runtime} {
+
+            kissb-llvm {
+
+                set outFolder $toolchainFolder/ghdl-${::ghdl.version}
+                files.requireOrRefresh $outFolder/bin/ghdl ghdl {
+                    files.inDirectory $toolchainFolder {
+                        files.downloadOrRefresh https://kissb.s3.de.io.cloud.ovh.net/hdl/ghdl/ghdl-${::ghdl.version}-llvm-rhel9.zip GHDL
+                        files.extract           ghdl-${::ghdl.version}-llvm-rhel9.zip
+                    }
+                }
+                files.makeExecutable $outFolder/bin/ghdl
+                files.makeExecutable $outFolder/bin/ghdl1-llvm
+                vars.set ghdl.installFolder $outFolder
+                env.set LD_LIBRARY_PATH $outFolder/bin
+            }
+
+            default {
+                log.fatal "Unknown GHDL runtime ${::ghdl.runtime}"
+            }
+        }
+
+
+    }
+
+    kissb.extension ghdl {
+
+        init.kissb-llvm  args {
+             kiss::toolchain::init ghdl
+        }
+
+        run args {
+
+            exec.run ${::ghdl.installFolder}/bin/ghdl {*}$args
+        }
+
+        analyze args {
+            ghdl.run -a {*}$args
+        }
+
+        elaborate args {
+            ghdl.run -e {*}$args
+        }
+
+        simulate args {
+            ghdl.run -r {*}$args
+        }
+
+        vrun {top sources} {
+
+        }
+    }
+}
